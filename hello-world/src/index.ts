2728147e8fe1333770f2136d06b2256b987f2466
Tomáš Valenta <valenta.and.thomas@gmail.com>
2021-02-11 01:42:33 +0100

    Fix variable casing in the hello-world example

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,29 +1,25 @@
-import {
-  ExtensionEventHandlers,
-  extensionEndpoints,
-} from '@devbookhq/extension';
+import Devbook, { ExtensionEventHandlers } from '@devbookhq/extension';
+
+// If you want to use our predefined functions to fetch extension data, you have to initialize the Devbook object.
+const devbook = new Devbook();
 
 const extensionEventHandlers: ExtensionEventHandlers = {
-  // This function will be called every time user changes their query in the Devbook search input.
-  onDidQueryChange: async (data) => {
+  // Called every time user changes the search query in the Devbook search input.
+  onDidQueryChange: async (data, extensionMode, token) => {
     // Mock example not fetching any data from the extension data.
     const results = [
       {
         id: '1',
-        data: {
-          title: 'Hello World!',
-          body: `The search query was ${data.query}`,
-        },
+        title: 'Hello World!',
+        body: `The search query was ${data.query}`,
       },
     ];
     return { results };
 
-    // You can fetch your extension data from https://api.usedevbook.com/v1/extension/:yourExtensionID/entry/query.
+    // Fetch your extension data from the https://api.usedevbook.com/:version/extension/:extensionID endpoint.
     // You can use our exported predefined functions for that:
 
-    // const results = await extensionEndpoints.searchExtensionData(data.query, {
-    //   indexes: ['testIndex'],
-    // });
+    // const results = await devbook.search(['testIndex'], data.query);
     // return { results };
   },
 }
